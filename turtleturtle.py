01e38b65eef268773499d4ed0ad2cccc383fe8f1
queenofhearts4171 <111077199+queenofhearts4171@users.noreply.github.com>
2022-09-13 13:55:53 -0400

    Added rand generating
    Random generating of numbers/images
    To-Add: Screenshot functionality.

--- turtleturtle.py
+++ turtleturtle.py
@@ -1,20 +1,31 @@
-import turtle
 import random
+from tkinter import *
+from turtle import *
+import turtle
+
 wn = turtle.Screen()
 wn.bgcolor('black')
-turtle.speed(1000)
+turtle.speed(0)
 
-colors = ['blue','red']
+colors = ['#C4E0F8','#F4A2E9']
+rand = random.randint(100,9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999*10*10)
 
-for i in range(800):
+
+for i in range(10):
     turtle.forward(5*i)
 
     #Replace with number in photo.
-    turtle.right(3.141)
-
+    turtle.right(rand)
+    print(i)
+	
     if i%2 == 0:
         turtle.color(colors[0])
     else:
         turtle.color(colors[1])
 
-turtle.exitonclick()
+
+
+
+
+
+
